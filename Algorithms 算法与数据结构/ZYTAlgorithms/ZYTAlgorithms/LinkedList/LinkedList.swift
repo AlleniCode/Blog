788182f4a1ae0be6a17743bf81ac4feef72f11e3
AllenZhang <zhangyutao2019@icloud.com>
2020-10-18 22:00:24 +0800

    Update

---
+++ Algorithms 算法与数据结构/ZYTAlgorithms/ZYTAlgorithms/LinkedList/LinkedList.swift
@@ -0,0 +1,63 @@
+//
+//  LinkedList.swift
+//  ZYTAlgorithms
+//
+//  Created by 张玉涛 on 2020/10/18.
+//
+
+import Foundation
+
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        if head == nil {
+            return nil
+        }
+        
+        if head?.next == nil {
+            return head
+        }
+        
+        let newHead = reverseList(head?.next)
+        
+        head?.next?.next = head
+        head?.next = nil
+        
+        return newHead
+    }
+    
+    func test() {
+        let node5 = ListNode(5)
+        node5.next = nil
+
+        let node4 = ListNode(4)
+        node4.next = node5
+
+        let node3 = ListNode(3)
+        node3.next = node4
+
+        let node2 = ListNode(2)
+        node2.next = node3
+
+        let node1 = ListNode(1)
+        node1.next = node2
+        
+        var newNode = reverseList(node1)
+        while newNode != nil {
+            print(newNode?.val ?? "")
+            newNode = newNode?.next
+        }
+        
+    }
+}
+
+
---
+++ Algorithms 算法与数据结构/ZYTAlgorithms/ZYTAlgorithms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZYTAlgorithms
+//
+//  Created by 张玉涛 on 2020/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
